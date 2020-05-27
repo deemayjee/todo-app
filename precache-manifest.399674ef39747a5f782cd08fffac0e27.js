e22adc4c8fd21d5207f8b26dc21eacb1f9af9b29
Dimeji Onatunde <deemayjee@Dimejis-MacBook-Pro.local>
2020-05-27 14:29:14 +0100

    Updates

---
+++ precache-manifest.399674ef39747a5f782cd08fffac0e27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b546cc312c8df8272ed93ccbcf79e1",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "9c97cbcaecd47714769e",
+    "url": "/todo-app/static/css/main.2cb5c20a.chunk.css"
+  },
+  {
+    "revision": "ab38089449e2bac7b28c",
+    "url": "/todo-app/static/js/2.30a5b71b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.30a5b71b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c97cbcaecd47714769e",
+    "url": "/todo-app/static/js/main.3ff08a13.chunk.js"
+  },
+  {
+    "revision": "672d1f4664f6508537da",
+    "url": "/todo-app/static/js/runtime-main.6751aa9a.js"
+  }
+]);
